70e9cad3d443e6dec4a2c95d58ac31c0ef3c4b40
Md Abdullah Al Mamun <mamun.cseian@gmail.com>
2021-05-30 09:12:58 +0600

    First matlab program

---
+++ myFirstMatlabProgram.m
@@ -0,0 +1,18 @@
+% a simple line of addition
+%
+4 + 5 % in volts
+%% this section deos some arithmetic
+(5+7-3) / (4-3)+ ...
+    17^2
+
+%{
+    multiline
+    comment
+    no problem with matlab
+
+%}
+ %% even simpler math
+ 
+ 4+3
+ 
+ 
